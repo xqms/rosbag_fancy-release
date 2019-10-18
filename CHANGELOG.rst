939ed6d4619e460cd7c7b1129a46b5c8a6f7925e
Max Schwarz <max.schwarz@online.de>
2019-10-19 00:11:30 +0200

    Imported upstream version '0.1.0' of 'upstream'

---
+++ CHANGELOG.rst
@@ -0,0 +1,8 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package rosbag_fancy
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.1.0 (2019-10-18)
+------------------
+* Initial release 
+* Contributors: Jan Quenzel, Max Schwarz
---
+++ src/main.cpp
@@ -0,0 +1,189 @@
+// Fancy rosbag terminal UI
+// Author: Max Schwarz <max.schwarz@uni-bonn.de>
+
+#include <boost/program_options.hpp>
+
+#include <ros/ros.h>
+#include <rosfmt/rosfmt.h>
+
+#include <rosbag/bag.h>
+
+#include "topic_manager.h"
+#include "message_queue.h"
+#include "bag_writer.h"
+#include "terminal.h"
+#include "topic_subscriber.h"
+#include "ui.h"
+
+namespace po = boost::program_options;
+using namespace rosbag_fancy;
+
+std::string timeToString(const ros::Time& cur_ros_t)
+{
+	std::time_t cur_t = cur_ros_t.sec;
+	std::stringstream ss;
+	ss << std::put_time(std::localtime(&cur_t), "%Y-%m-%d-%H-%M-%S");
+	return ss.str();
+}
+
+int record(const std::vector<std::string>& options)
+{
+	po::variables_map vm;
+
+	// Handle CLI arguments
+	{
+		po::options_description desc("Options");
+		desc.add_options()
+			("help", "Display this help message")
+			("prefix,p", po::value<std::string>()->default_value("bag"), "Prefix for output bag file. The prefix is extended with a timestamp.")
+			("output,o", po::value<std::string>(), "Output bag file (overrides --prefix)")
+			("topic", po::value<std::vector<std::string>>()->required(), "Topics to record")
+			("queue-size", po::value<std::uint64_t>()->default_value(500ULL*1024*1024), "Queue size in bytes")
+		;
+
+		po::positional_options_description p;
+		p.add("topic", -1);
+
+		auto usage = [&](){
+			std::cout << "Usage: rosbag_fancy record [options] -o <bag file> <topics...>\n\n";
+			std::cout << desc << "\n\n";
+			std::cout << "Topics may be annotated with a rate limit in Hz, e.g.:\n";
+			std::cout << "  rosbag_fancy /camera/image_raw=10.0\n";
+			std::cout << "\n";
+		};
+
+		try
+		{
+			po::store(
+				po::command_line_parser(options).options(desc).positional(p).run(),
+				vm
+			);
+
+			if(vm.count("help"))
+			{
+				usage();
+				return 0;
+			}
+
+			po::notify(vm);
+		}
+		catch(po::error& e)
+		{
+			std::cerr << "Could not parse arguments: " << e.what() << "\n\n";
+			usage();
+			return 1;
+		}
+	}
+
+	ros::NodeHandle nh{"~"};
+
+	std::vector<std::string> topics = vm["topic"].as<std::vector<std::string>>();
+	std::sort(topics.begin(), topics.end());
+
+	TopicManager topicManager;
+	for(auto& topicSpec : topics)
+	{
+		std::string name = topicSpec;
+		float rateLimit = 0.0f;
+
+		auto sepIdx = topicSpec.find('=');
+
+		if(sepIdx != std::string::npos)
+		{
+			name = topicSpec.substr(0, sepIdx);
+
+			try
+			{
+				rateLimit = boost::lexical_cast<float>(topicSpec.substr(sepIdx+1));
+			}
+			catch(boost::bad_lexical_cast&)
+			{
+				std::cerr << "Bad topic spec: '" << topicSpec << "'\n";
+				return 1;
+			}
+		}
+
+		topicManager.addTopic(name, rateLimit);
+	}
+
+	MessageQueue queue{vm["queue-size"].as<std::uint64_t>()};
+	BagWriter writer{queue};
+
+	// Figure out the output file name
+	std::string bagName = "";
+	if(vm.count("output"))
+		bagName = vm["output"].as<std::string>();
+	else
+	{
+		std::string prefix = vm["prefix"].as<std::string>();
+
+		bagName = fmt::format("{}_{}.bag",
+			vm["prefix"].as<std::string>(),
+			timeToString(ros::Time::now())
+		);
+	}
+	ROSFMT_INFO("Bagfile name: {}", bagName);
+
+	try
+	{
+		writer.start(bagName);
+	}
+	catch(rosbag::BagException& e)
+	{
+		ROSFMT_ERROR("Could not open output bag file: %s", e.what());
+		return 1;
+	}
+
+	TopicSubscriber subscriber{topicManager, queue};
+
+	UI ui{topicManager, queue, writer, UI::Mode::Recording};
+
+	ros::spin();
+
+	return 0;
+}
+
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "rosbag_fancy", ros::init_options::AnonymousName);
+
+	auto usage = [](std::FILE* f){
+		fmt::print(f,
+			"Usage: rosbag_fancy <command> [args]\n\n"
+			"Available commands:\n"
+			"  record: Record a bagfile\n"
+			"\n"
+			"See rosbag_fancy <command> --help for command-specific instructions.\n"
+			"\n"
+		);
+	};
+
+	if(argc < 2)
+	{
+		usage(stderr);
+		return 1;
+	}
+
+	std::string cmd = std::string(argv[1]);
+	std::vector<std::string> arguments(argc - 2);
+	std::copy(argv + 2, argv + argc, arguments.begin());
+
+	if(cmd == "-h" || cmd == "--help")
+	{
+		usage(stdout);
+		return 0;
+	}
+
+	if(cmd == "record")
+		return record(arguments);
+	else
+	{
+		fmt::print(stderr, "Unknown command {}, see --help\n", cmd);
+		return 1;
+	}
+
+	return 0;
+}
+
