07ea70267fdf705dcc4a53f806b9a09bcf54ebcf
Max Schwarz <max.schwarz@ais.uni-bonn.de>
2023-07-10 14:43:55 +0200

    Imported upstream version '1.0.0' of 'upstream'

--- README.md
+++ README.md
@@ -2,18 +2,37 @@
 rosbag_fancy
 ============
 
-![animation](https://xqms.github.io/rosbag_fancy/anim.svg)
+<img src="https://xqms.github.io/rosbag_fancy/anim2.svg" width="100%" />
 
 `rosbag_fancy` is a fancy terminal UI frontend for the venerable [rosbag]
 tool.
 
-At the moment, only the `record` command is implemented. It offers the following
-advantages over plain `rosbag record`:
+Recording
+---------
+
+At the moment, the `record` command is `rosbag_fancy`'s main feature.
+It offers the following advantages over plain `rosbag record`:
 
  * Live display of statistics per topic, such as number of messages, bandwidth,
    dropped messages, etc. Never notice *after* recording that you misspelled a
    topic name!
  * Bash completion for topic names
  * Optional per-topic rate limiting
+
+Info
+----
+
+`rosbag_fancy` also offers an `info` command similar to `rosbag info`, which
+has the same features but is much faster.
+
+Playback
+--------
+
+Using `rosbag_fancy play <bagfile>` you can play bag files interactively.
+Similar to `info`, this is much faster than plain `rosbag`.
+
+As an additional feature, `rosbag_fancy play` aggregates the `tf_static`
+topic over time, so no matter how many tf publishers were active or how
+much you seek in the file, the static transforms will be kept up-to-date.
 
 [rosbag]: http://wiki.ros.org/rosbag
---
+++ rosbag_fancy/src/main.cpp
@@ -0,0 +1,66 @@
+// Fancy rosbag terminal UI
+// Author: Max Schwarz <max.schwarz@uni-bonn.de>
+
+#include <boost/program_options.hpp>
+
+#include <ros/init.h>
+
+#include <rosfmt/full.h>
+
+// Implemented in cmd_*.cpp
+int record(const std::vector<std::string>& options);
+int info(const std::vector<std::string>& options);
+int play(const std::vector<std::string>& options);
+int test(const std::vector<std::string>& options);
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "rosbag_fancy", ros::init_options::AnonymousName);
+
+	auto usage = [](std::FILE* f){
+		fmt::print(f,
+			"Usage: rosbag_fancy <command> [args]\n\n"
+			"Available commands:\n"
+			"  record: Record a bagfile\n"
+			"  info: Display information about a bagfile\n"
+			"  play: Play bagfile\n"
+			"  test: Run internal unit-tests\n"
+			"\n"
+			"See rosbag_fancy <command> --help for command-specific instructions.\n"
+			"\n"
+		);
+	};
+
+	if(argc < 2)
+	{
+		usage(stderr);
+		return 1;
+	}
+
+	std::string cmd = std::string(argv[1]);
+	std::vector<std::string> arguments(argc - 2);
+	std::copy(argv + 2, argv + argc, arguments.begin());
+
+	if(cmd == "-h" || cmd == "--help")
+	{
+		usage(stdout);
+		return 0;
+	}
+
+	if(cmd == "record")
+		return record(arguments);
+	else if(cmd == "info")
+		return info(arguments);
+	else if(cmd == "play")
+		return play(arguments);
+	else if(cmd == "test")
+		return test(arguments);
+	else
+	{
+		fmt::print(stderr, "Unknown command {}, see --help\n", cmd);
+		return 1;
+	}
+
+	return 0;
+}
+
